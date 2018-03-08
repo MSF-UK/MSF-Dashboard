cc93b5bc1e4dcf1fcae166199e58456b05b48316
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2018-03-08 21:41:09 +0000

    bug fix

--- src_core/js/module-datacheck.js
+++ src_core/js/module-datacheck.js
@@ -739,7 +739,7 @@ epiwk: function(rec,key,none){
 		// Surveillance
 		//------------------------------------------------------------------------------------
 		// If disease array is provided empty
-		if(!(module_datacheck.testvalue.empty(rec,'disease','none')) && (g.medical_diseaseslist.indexOf(rec[g.medical_headerlist.disease].trim()) == -1) && g.module_datacheck.diseasecheck ){
+		if(!(module_datacheck.testvalue.empty(rec,'disease','none')) && (g.medical_diseaseslist.indexOf(rec[g.medical_headerlist.disease].trim().split('_').join(' ')) == -1) && g.module_datacheck.diseasecheck ){
 			g.medical_diseaseslist.push(toTitleCase(rec[g.medical_headerlist.disease].trim().split('_').join(' ')));
 		}
 
